2ca16e22839efda1a426345e26c83c1a5b428fa5
Kentaro <kenken@whim.jp>
2019-05-21 22:48:36 +0900

    Initial Commit

---
+++ INIAD IoT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INIAD IoT
+//
+//  Created by Kentaro on 2019/05/21.
+//  Copyright © 2019 Kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
